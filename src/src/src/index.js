f482c32739a396bb162dd6a7589d29df4dfa6235
hnk800 <67208156+hnk800@users.noreply.github.com>
2025-02-25 20:08:33 +0900

    Create index.js

---
+++ src/src/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WeightLossCalculator from './WeightLossCalculator';
+import './index.css';
+
+ReactDOM.render(<WeightLossCalculator />, document.getElementById('root'));
